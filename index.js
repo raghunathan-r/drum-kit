951f71b156a83eb2f84fef59c225e800481e883d
raghunathan-r <50546911+raghunathan-r@users.noreply.github.com>
2020-12-25 00:08:15 +0530

    Added button press animation

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ for( var i = 0; i < document.querySelectorAll(".drum").length ; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
         buttonInnerHTML = this.innerHTML;
         makeSound(buttonInnerHTML);
+        buttonAnnimation(buttonInnerHTML);
     } );
 }
 
@@ -13,10 +14,11 @@ for( var i = 0; i < document.querySelectorAll(".drum").length ; i++) {
 document.addEventListener("keypress", function(event) {
     keyPressed = event.key;
     makeSound(keyPressed);
+    buttonAnnimation(keyPressed);
 })
 
-function makeSound (key) {
-    switch (key) {
+function makeSound (keyPressed) {
+    switch (keyPressed) {
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play(); 
@@ -48,4 +50,13 @@ function makeSound (key) {
         default:
             break;
     }   
+}
+
+function buttonAnnimation (currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+    }, 100);
 }
